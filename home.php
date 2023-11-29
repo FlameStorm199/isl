7e83271e57d437234f0f06d6168adf36cd1534e0
Alessio <102922085+alec0r@users.noreply.github.com>
2023-11-29 15:03:30 +0100

    home

---
+++ home.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html>
+
+<head>
+    <title>ISLogistics</title>
+    <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+    <link rel="stylesheet" href="css/login_style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href='https://fonts.googleapis.com/css?family=Rubik' rel='stylesheet'>
+</head>
+
+<body>
+    <!-- EG: Aggiungere qualche elemento per fare l'header -->
+    <nav class="navbar bg-color">
+        <a class="navbar-brand" href="#">
+            <img src="img/nav-logo-removebg.png" id="logo-img" alt="">
+            <a href="logout.php" style="text-decoration: none; color: white;">Logout</a>
+        </a>
+        <!-- <div class="help"><img src="img/settings.png" alt="settings icon"></div> -->
+    </nav>
+    <div id="hi_user">
+        <h1>Buongiorno User</h1>
+        <!-- Inserire user appena loggato -->
+    </div>
+</body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+
+</html>
